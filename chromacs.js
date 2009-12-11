ee0366fcc815141eff4361bf6f4f48c8c4911210
Phil Jackson <phil@shellarchive.co.uk>
2009-12-11 21:36:10 +0000

    Basic chaining in place.

--- chromacs.js
+++ chromacs.js
@@ -1,31 +1,53 @@
 // tell chrome to listen for a keypress
 document.addEventListener( "keydown", key_press, false );
 
-var bindings = {
-    "R": function () { location.reload() }
+var chromacs_bindings = {
+    "r": function () { location.reload() },
+    "B": function () { history.back()    },
+    "F": function () { history.forward() },
+    "C-x": {
+        "C-f": function () { alert( "chained!" ) }
+    }
 };
 
+var current_binding = chromacs_bindings;
+
 function key_press( e ) {
-    var key = get_key( e );
-    var command = bindings[ key ];
+    var key = get_key( e ),
+    command = current_binding[ key ];
+
+    console.log( key );
 
     if ( command ) {
-        console.log( "hit" );
+        console.log( typeof command );
 
-        command();
+        switch ( typeof command ) {
+        case "function":
+            // reset the key-chain
+            current_binding = chromacs_bindings;
 
-        // nothing else, thanks
+            // run the function
+            command();
+
+            break;
+        case "object":
+            // set the current binding to that of the chain
+            current_binding = command;
+
+            break;
+        }
+
         e.preventDefault();
     }
 }
 
 function get_key( e ) {
-    var key = e.keyIdentifier,
+    var key = key   = String.fromCharCode( e.keyCode ),
 
-    // emacs style key identifiers
-    ctrl = e.ctrlKey                 ? 'C-' : '',
-    meta = ( e.metaKey || e.altKey ) ? 'M-' : '',
-    shift = e.shiftKey               ? 'S-' : '';
+    // try and make the identifiers as emacs-like as possible
+    ctrl  = e.ctrlKey                 ? 'C-' : '',
+    meta  = ( e.metaKey || e.altKey ) ? 'M-' : '',
+    shift = e.shiftKey                ? 'S-' : '';
 
-    return ctrl + meta + String.fromCharCode( e.keyCode );
+    return ctrl + meta + ( shift ? key : key.toLowerCase() );
 }
